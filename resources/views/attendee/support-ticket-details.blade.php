92dedcef4efb5819f98311c74c46db84363507cd
Ning <wen.ninq@gmail.com>
2017-03-27 15:37:57 +0800

    Modifies support ticket details UI

--- resources/views/attendee/support-ticket-details.blade.php
+++ resources/views/attendee/support-ticket-details.blade.php
@@ -18,49 +18,59 @@
             <h3 class="ui teal dividing header">
                 Ticket Details
             </h3>
+            <div class="ui stackable grid">
+                <div class="sixteen wide mobile sixteen wide tablet fourteen wide computer fourteen wide large screen column">
+                    <div align="right">Has this issue been resolved? <a href="#"> Close It</a></div>
+                    <h4 class="header">
+                        <p>Ticket No. {{ $post->ticket_number }}</p>
+                        <p>Title : {{ $post->title }}</p>
+                    </h4>
+                    <div class="ui message">
+                        <div class="ui comments">
+                            <div class="comment">
+                                <a class="avatar teal large ui label">{{ substr($post->user->first_name, 0, 1) }}</a>
 
-            <h4 class="header">Ticket No. {{ $post->ticket_number }}</h4>
-            <h4 class="header">Title : {{ $post->title }}</h4>
+                                <div class="content">
+                                    <a class="author">{{ $post->user->first_name }}</a>
+                                    <div class="metadata">
+                                        <span class="date">{{ $post->created_at->format('M d, Y h:i A') .' | '. $post->created_at->diffForHumans()}}</span>
+                                    </div>
+                                    <div class="text">
+                                        {{ $post->message }}
+                                    </div>
 
-            <div class="ui message">
-                <div class="ui comments">
-                    <div class="comment">
-                        <a class="avatar teal large ui label">{{ substr($post->user->first_name, 0, 1) }}</a>
-
-                        <div class="content">
-                            <a class="author">{{ $post->user->first_name }}</a>
-                            <div class="metadata">
-                                <span class="date">{{ $post->created_at }}</span>
-                            </div>
-                            <div class="text">
-                                {{ $post->message }}
-                            </div>
-
-                            <div class="ui comments">
-                                <div class="comment">
-                                    <a class="avatar orange large ui label">A</a>
-                                    <div class="content">
-                                        <a class="author">Admin #1</a>
-                                        <div class="metadata">
-                                            <span class="date">Mar 11, at 5:42PM</span>
-                                        </div>
-                                        <div class="text">
-                                            Yes
+                                    <div class="ui comments">
+                                        <div class="comment">
+                                            <a class="avatar orange large ui label">A</a>
+                                            <div class="content">
+                                                <a class="author">Admin #1</a>
+                                                <div class="metadata">
+                                                    <span class="date">Mar 11, at 5:42PM</span>
+                                                </div>
+                                                <div class="text">
+                                                    Yes
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="comment">
-                            <a class="avatar"><img src="http://placehold.it/15x15"></a>
+                                <div class="comment">
+                                    <a class="avatar"><img src="http://placehold.it/15x15"></a>
 
-                            <div class="content">
-                                <a class="author">User Name</a>
-                                <div class="metadata">
-                                    <span class="date">Mar 11, at 5:47PM</span>
-                                </div>
-                                <div class="text">
-                                    Ok
+                                    <div class="content">
+                                        <a class="author">User Name</a>
+                                        <div class="metadata">
+                                            <span class="date">Mar 11, at 5:47PM</span>
+                                        </div>
+                                        <div class="text">
+                                            <form class="ui form">
+                                                <div class="field">
+                                                    <textarea rows="2"></textarea>
+                                                </div>
+                                                <button class="ui mini blue basic button">Add Reply</button>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
