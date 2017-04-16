a2098827a2460275ea22fb805a3054e12d83869f
Ning <wen.ninq@gmail.com>
2017-04-17 00:15:52 +0800

    Adds footer

--- resources/views/guest/footer.blade.php
+++ resources/views/guest/footer.blade.php
@@ -1,14 +1,43 @@
 <div class="ui inverted vertical footer segment">
-    <div class="ui centered stackable grid">
-        <div class="sixteen wide mobile sixteen wide tablet sixteen wide computer sixteen wide large screen column">
-            ShineS ServiceS {{ date("Y") }}. All Rights Reserved
-            <div class="sixteen wide mobile sixteen wide tablet sixteen wide computer sixteen wide large screen column">
-                <div class="ui inverted dividing"></div>
-                <span>Share with friends: </span>
-                <i class="teal circular facebook f icon link" data-content="Facebook"></i>
-                <i class="teal circular google icon link" data-content="Google+"></i>
-                <i class="teal circular twitter icon link" data-content="Twitter"></i>
+    <div class="ui container">
+        <div class="ui stackable inverted divided grid">
+            <div class="three wide column">
+                <h4 class="ui inverted header">General Information</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item">Home</a>
+                    <a href="#" class="item">About Us</a>
+                    <a href="#" class="item">Contact Us</a>
+                    <a href="#" class="item">Browse Events</a>
+                </div>
+            </div>
+            <div class="three wide column">
+                <h4 class="ui inverted header">Services</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item">Forgot Password?</a>
+                    <a href="#" class="item">Unsunscribe</a>
+                    <a href="#" class="item">FAQ</a>
+                    <a href="#" class="item">Purchase Ticket</a>
+                </div>
+            </div>
+            <div class="three wide column">
+                <h4 class="ui inverted header">Connect With Us</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item"><i class="teal facebook f icon link"></i>Facebook</a>
+                    <a href="#" class="item"><i class="teal google icon link"></i>Google+</a>
+                    <a href="#" class="item"><i class="teal twitter icon link"></i>Twitter</a>
+                    <a href="#" class="item"><i class="teal linkedin icon link"></i>LinkedIn</a>
+                </div>
+            </div>
+            <div class="seven wide column">
+                <div class="ui horizontal inverted divided link list">
+                    <a href="#" class="item">Site Map</a>
+                    <a href="#" class="item">Help</a>
+                    <a href="#" class="item">Terms & Conditions</a>
+                    <a href="#" class="item">Privacy Policy</a>
+                </div>
             </div>
         </div>
+        <div class="ui inverted section divider"></div>
+        <div class="ui center aligned basic inverted segment">Copyright 2017</div>
     </div>
 </div>
