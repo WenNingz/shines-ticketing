9eaae49dfd68c68065802b1e96ffdde58cede0c5
Ning <wen.ninq@gmail.com>
2017-04-17 00:14:54 +0800

    Modifies purchases table
    Removes ticket id column

--- database/migrations/2017_03_18_125146_create_purchases_table.php
+++ database/migrations/2017_03_18_125146_create_purchases_table.php
@@ -6,28 +6,16 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePurchasesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('ticket_id')->unsigned();
-            $table->foreign('ticket_id')->references('id')->on('tickets');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('purchases');
