41d525d456adfa3df2b3045e5ae489b9327056b1
Ning <wen.ninq@gmail.com>
2017-04-22 13:11:12 +0800

    Modifies success message layout
    - Adds header

--- resources/views/layout/success.blade.php
+++ resources/views/layout/success.blade.php
@@ -1,5 +1,8 @@
 @if (session('status'))
     <div class="ui positive message">
-        {{ session('status') }}
+        @if(session('header'))
+            <div class="header">{{ session('header') }}</div>
+        @endif
+        <p>{{ session('status') }}</p>
     </div>
 @endif
